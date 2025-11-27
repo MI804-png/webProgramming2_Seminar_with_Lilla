7e43eda7ee41202a936aa0850e8e8e53b86b0901
Mikhael <mikha.nabil13@gmail.com>
2025-11-28 00:38:10 +0200

    Fix ecosystem config to use start.js instead of start-homework-style.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'app206',
-      script: 'start-homework-style.js',
+      script: 'start.js',
       instances: 1,
       autorestart: true,
       watch: false,
